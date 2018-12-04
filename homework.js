6f8982f0ba164e5e35cb955cb4e624ebac741213
Sam LoCascio <sflocascio@gmail.com>
2018-12-03 23:14:22 -0500

    commit of wk6d1 assignment

--- homework.js
+++ homework.js
@@ -1,26 +1,62 @@
 // 1. Set the variable `name` to the string "Addison".
+let givenName = "Addison"
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
 
+const candies = 20
+const people = 6
+const leftover = (20 % 6) 
+
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+
+function greeting(name) {
+    return `Hello, ${name}!`
+}
+
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
 
+function isOdd(number) {
+    if (number%2 === 1 || number%2 === -1)
+        return true;
+    else 
+        return false;
+}
+
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
 
+function isEven(number) {
+    if (number%2 === 0)
+        return true 
+    else 
+        return false 
+}
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
 
+function fahrenheitToCelsius(temp) {
+    
+    let conversion = (temp - 32) * 5/9
+    return conversion
+    
+}
+
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+
+function celsiusToFahrenheit(temp) {
+    let conversion = (temp * 9/5) + 32
+    return conversion
+
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -29,9 +65,25 @@
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
+function fahrenheitToKelvin(ftemperature) {
+
+    let tempCelsius = fahrenheitToCelsius(ftemperature)
+    let tempKelvin = tempCelsius + 273.15
+
+    return tempKelvin
+}
+
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+
+function lesser(x, y) {
+
+    if (x <= y)
+        return x
+    else 
+        return y 
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -45,14 +97,63 @@
 //
 // If any other language code is used, return nothing.
 
+function multigreeting (name, langCode) {
+    if (langCode === 'en' )
+        return `Hello, ${name}!`
+    if (langCode === 'es' )
+        return `¡Hola, ${name}!`
+    if (langCode === 'fr' )
+        return `Bonjour, ${name}!`
+    if (langCode === 'eo' )
+        return `Saluton, ${name}!`
+    else 
+        return undefined
+}
+
 // 10. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+
+function sum(array){
+    let s = 0;
+    console.log("hello")
+    for (let index= 0; index<array.length; index++){
+        s = s + array[index];
+    }
+    return s 
+}
 
 // 11. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
 
+function average(array){
+    let s = 0;
+    console.log("hello")
+    for (let index= 0; index<array.length; index++){
+        s = s + array[index];
+    }
+    average = s/array.length
+    if (s === 0)
+        return undefined
+    else 
+        return average
+}
+
 // 12. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+
+function minimum(array){
+    let s = [];
+    
+    for (let index= 0; index<array.length; index++){
+        if (array[index] < array[index+1])
+            return s.push(array[index])
+        else
+            return s.push(array[index+1])
+    }
+    
+    return s
+}
+
 
 // 13. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
